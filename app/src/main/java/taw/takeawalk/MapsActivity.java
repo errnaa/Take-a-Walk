5add7df5b1aae6c20bb9b7decab010f445e9bd12
errnaa <sirapobnanthayapirom@hotmail.com>
2016-04-24 11:56:03 -0500

    Changes to added call functionalities to Map Activity. Added permission to make phone calls in Manifest.

--- app/src/main/java/taw/takeawalk/MapsActivity.java
+++ app/src/main/java/taw/takeawalk/MapsActivity.java
@@ -1,8 +1,11 @@
 package taw.takeawalk;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,6 +13,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -23,6 +28,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Button nineOneOneButton = (Button) findViewById(R.id.nineOneOneButton);
+        nineOneOneButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:911"));
+                try {
+                    startActivity(callIntent);
+                } catch (SecurityException e) {
+
+                }
+                return true;
+            }
+        });
+
+        Button safeRideButton = (Button) findViewById(R.id.saferideButton);
+        safeRideButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:2172657433"));
+                try {
+                    startActivity(callIntent);
+                } catch (SecurityException e) {
+
+                }
+                return true;
+            }
+        });
     }
 
 
@@ -49,10 +82,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seibel, 15.0f));
             LatLng illini = new LatLng(40.109625, -88.227256);
             mMap.addMarker(new MarkerOptions().position(illini));
+
+            PolylineOptions rectOptions = new PolylineOptions()
+                    .add(new LatLng(40.114501, -88.223998))
+                    .add(new LatLng(40.110562, -88.223955))
+                    .add(new LatLng(40.110373, -88.227742));
+
+            Polyline polyline = mMap.addPolyline(rectOptions);
         } else {
             LatLng burnham = new LatLng(40.113091, -88.234671);
             mMap.addMarker(new MarkerOptions().position(burnham));
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(burnham, 15.0f));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(burnham, 16.0f));
+
+            PolylineOptions rectOptions = new PolylineOptions()
+                    .add(new LatLng(40.112673, -88.234697))
+                    .add(new LatLng(40.112686, -88.232082))
+                    .add(new LatLng(40.111488, -88.232072))
+                    .add(new LatLng(40.111488, -88.235387))
+                    .add(new LatLng(40.112686, -88.235419))
+                    .add(new LatLng(40.112673, -88.234697));
+
+            Polyline polyline = mMap.addPolyline(rectOptions);
         }
     }
 }
