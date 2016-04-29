19c85281576b625643b1fe5212dac63084d87f97
errnaa <sirapobnanthayapirom@hotmail.com>
2016-04-29 06:14:40 -0500

    Added more functionality that was missing,

--- app/src/main/java/taw/takeawalk/ContactsActivity.java
+++ app/src/main/java/taw/takeawalk/ContactsActivity.java
@@ -1,8 +1,13 @@
 package taw.takeawalk;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ContactsActivity extends Activity {
 
@@ -10,5 +15,27 @@ public class ContactsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+    }
+
+    public void call(View view) {
+        Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:2170000000"));
+        try {
+            startActivity(callIntent);
+        } catch (SecurityException e) {
+
+        }
+    }
+
+    public void message(View view) {
+        AlertDialog alertDialog = new AlertDialog.Builder(ContactsActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("Message(s) sent!");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+        alertDialog.show();
     }
 }
